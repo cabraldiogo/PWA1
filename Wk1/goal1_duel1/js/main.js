2ab1ef77f425695b99724331f261f6def8bf77ee
cabraldiogo <cabraldiogo@me.com>
2013-10-01 15:02:21 -0400

    Goal1FolderCreation
    I just created all the folders, index and js file as requested on the
    assignment instruction

---
+++ Wk1/goal1_duel1/js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: diogocabral
+ * Date: 10/1/13
+ * Time: 2:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+//Diogo Cabral - October, 01 - Goal 1 - Duel1 Assignment
+console.log("Js Working");
+
+
+
