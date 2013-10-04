a598a2bea4ace9c6ed8c29f58db3ab31e399a32f
cabraldiogo <cabraldiogo@me.com>
2013-10-04 18:40:23 -0400

    update
    I did the 3 first exercises. I am going to try a modification on
    exercise 2. I will try to put the variables inside the function

--- goal2_assign_jspractice/js/init.js
+++ goal2_assign_jspractice/js/init.js
@@ -28,7 +28,71 @@ function avgNumbers(numArray){
 
 };
 
-var average = avgNumbers(numbers);
+var average = "The average of my array is " + (avgNumbers(numbers)).toFixed(2);
 console.log(average);
+
+//==================================//
+
+// create a function named 'fullName'
+// accept 2 parameters into the function that are strings (firstname and lastname)
+// return the name after it has been concatenated
+// console.log the answer outside of the function
+
+var fName = "Diogo";
+var lName = "Cabral";
+
+function fullName (first,last){
+
+    var concName = fName +" "+ lName;
+    return concName
+
+}
+
+console.log(fullName(fName,lName));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//==================================//
+
+//create a function named 'wordCount'
+//accept 1 parameter into the function that is a long string of text words
+//create a function that counts all the words and return the answer
+//console.log the answer outside of the function
+
+
+var string = "I still have questions about functions and loops!";
+
+function wordCount(sentence){
+    var counter = 0;
+    for(var i= 0; i<= sentence.length; i++){
+        if(sentence.charAt(i) == " "){
+            counter ++;
+        }
+    }
+    return counter + 1;
+ }
+console.log("My sentence has " + wordCount(string)+ " words");
+
+//==================================//
+
+
+
+
+
 
 
