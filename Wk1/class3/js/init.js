423bd909d4c4fff9b5686989e141c7af52101c9b
cabraldiogo <cabraldiogo@me.com>
2013-10-05 09:23:20 -0400

    classActivity
    class practice

---
+++ Wk1/class3/js/init.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: diogocabral
+ * Date: 10/5/13
+ * Time: 9:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+//CLASS 3 ACTIVITY
