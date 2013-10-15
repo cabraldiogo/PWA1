20c6b4e9e9c37a5347b6a3c444fd4b8480148e94
cabraldiogo <cabraldiogo@me.com>
2013-10-15 19:45:34 -0400

    comments on the code

--- goal4_assign_guessing_game/js/main.js
+++ goal4_assign_guessing_game/js/main.js
@@ -15,20 +15,22 @@
     var guesses = 0;
     var statusInfo = "";
 
-
+// object that contain all the html elements
     var dom = {
 
         input: document.querySelector("#input"),
         output: document.querySelector("#output"),
         button: document.querySelector("#btn")
-
     };
 
+    //Generates a number between 1 and 10
     var genNumber = Math.floor(Math.random()* 10 +1);
     //console.log(genNumber);
 
+    //Event that calls the clickFn function enter the button is clicked
     dom.button.addEventListener("click",clickFn);
 
+    //Function to disable the button when the game is over
     function disableButton(){
         dom.button.removeEventListener("click",clickFn);
         console.log("disable button");
@@ -36,10 +38,12 @@
 
     function clickFn(){
 
-        console.log("click function");
+        //console.log(dom.input.value);
+        //console.log("click function");
         validation();
     }
 
+    //function that validates if the input is a number between 1 and 10. If so, this function calls the play function
     function validation(){
 
         inputNumber = parseInt(dom.input.value);
@@ -58,7 +62,8 @@
 
 
     }
-
+    //function play. It increases the guesses number, decreases the chances remaining, and give messages if the guess
+    // number is too low or too high. If the guess number matchs the random number or if the player uses 3 chances, the game is over
     function play(){
             chancesRemain--;
             guesses++;
