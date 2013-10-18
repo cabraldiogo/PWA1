3e9fa777f485716daa8b6d1040049dc1147abd7c
cabraldiogo <cabraldiogo@me.com>
2013-10-18 11:25:01 -0400

    inical variables and object

--- goal7_assign_objects/js/main.js
+++ goal7_assign_objects/js/main.js
@@ -4,5 +4,40 @@
  * Goal: Goal7
  */
 
+
+(function(){
 console.log("Main Js running");
 
+
+    var names = ["Diogo", "Gabriel", "Kareem", "Antonio", "Scott"];
+    var people = [];
+
+   function Person(n,r){
+       this.name = n;
+       this.row = r
+   }
+
+    var person = new Person(names[0],1);
+    console.log("Name: "+ person.name + "\n"+"Row #: "+ person.row);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
