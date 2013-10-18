658baf09d138c3a3a8a96025261d57add41e51f1
cabraldiogo <cabraldiogo@me.com>
2013-10-18 10:55:27 -0400

    Folders and files set up

---
+++ goal7_assign_objects/js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ */
+
+console.log("Main Js running");
+
---
+++ goal7_assign_objects/js/person.js
@@ -0,0 +1,7 @@
+/**
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ */
+
+console.log("Person Js running");
