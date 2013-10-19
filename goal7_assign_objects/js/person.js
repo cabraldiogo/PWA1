9962f21b0f5e37bccd9bfb64fef8008fa813ac84
cabraldiogo <cabraldiogo@me.com>
2013-10-19 15:14:19 -0400

    comments on code

--- goal7_assign_objects/js/person.js
+++ goal7_assign_objects/js/person.js
@@ -11,9 +11,11 @@
 (function(){
 
 
-    Person.jobs = ["Web Developer","Web Designer","Graphic Designer", "Film Maker", "Sound Engineer"];
+    //Person.jobs = ["Web Developer","Web Designer","Graphic Designer", "Film Maker", "Sound Engineer"];
+    var jobs = ["Web Developer","Web Designer","Graphic Designer", "Film Maker", "Sound Engineer"];
     Person.actions = ["Running","Sleeping", "Eating", "Drawing","Walking","Studying","Reading", "Singing"];
 
+    //This protopyte randomly changes the actions
     Person.prototype.update = function(){
         this.action = Person.actions[Math.floor(Math.random()*Person.actions.length)];
 
@@ -22,17 +24,13 @@
 
 
     function Person(n,r){
-    this.name = n;
-    this.row = r;
-    this.job = Person.jobs[Math.floor(Math.random()*Person.jobs.length)];
-    this.action = Person.actions[Math.floor(Math.random()*Person.actions.length)];
+        this.name = n;
+        this.row = r;
+        this.job = jobs[Math.floor(Math.random()*jobs.length)];
+        this.action = Person.actions[Math.floor(Math.random()*Person.actions.length)];
     }
 
-
-
+    //This command makes the Person object global
     window.Person = Person;
-
-
-
 
 })();
