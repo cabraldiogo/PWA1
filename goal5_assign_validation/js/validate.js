a50621861a5d29cc39a5ddee62848963bfcc212b
cabraldiogo <cabraldiogo@me.com>
2013-10-21 21:33:20 -0400

    Strugguling

--- goal5_assign_validation/js/validate.js
+++ goal5_assign_validation/js/validate.js
@@ -3,15 +3,22 @@
      Date: 1306
 */
 
+// Diogo Cabral | October, 21st
+
+
+
 (function(){
 
     myform.onsubmit = function(e){
 
         //Below is one example of the validateField call with an argument.
         //You must dynamically retrieve the ID name from the DOM/HTML.
+        //console.log("clicking");
+        e.name = document.querySelector("#f_username");
 
-        validateField(id);  //id = is the form input field ID
 
+        validateField("#f_username");  //id = is the form input field ID
+        console.log("f_username");
 
         e.preventDefault();
         return false;
@@ -19,8 +26,10 @@
 
 
     var validateField = function(inputName){
+        console.log("clicking");
 
-        if (inputName.name === 'id name goes here'){
+        if (inputName.name === "#f_username"){
+            console.log("If is true");
             var pattern = 'a RegEx pattern goes here'
 
             //You will need to create an else-if statement for each input field id.  The
