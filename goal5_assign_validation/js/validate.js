ad3ba5c27abae32e758bbab4747aca8765b73223
cabraldiogo <cabraldiogo@me.com>
2013-10-22 09:13:27 -0400

    RegExp reasearch
    I did some research on that, but I will wait to see the subject in class

--- goal5_assign_validation/js/validate.js
+++ goal5_assign_validation/js/validate.js
@@ -9,16 +9,29 @@
 
 (function(){
 
+
+
     myform.onsubmit = function(e){
 
         //Below is one example of the validateField call with an argument.
         //You must dynamically retrieve the ID name from the DOM/HTML.
         //console.log("clicking");
         e.name = document.querySelector("#f_username");
+        e.email = document.querySelector("#f_email");
+        e.phone = document.querySelector("#f_phone");
+        e.password = document.querySelector("#f_password");
+        e.ssn = document.querySelector("#f_ssn");
 
+        console.log("Onsubmit function middle");
 
-        validateField("#f_username");  //id = is the form input field ID
-        console.log("f_username");
+        validateField(e.name);  //id = is the form input field ID
+        validateField(e.email);
+        validateField(e.phone);
+        validateField(e.password);
+        validateField(e.ssn);
+
+        console.log("Onsubmit function final");
+
 
         e.preventDefault();
         return false;
@@ -28,7 +41,7 @@
     var validateField = function(inputName){
         console.log("clicking");
 
-        if (inputName.name === "#f_username"){
+        if (inputName.name === "#f_username") {
             console.log("If is true");
             var pattern = 'a RegEx pattern goes here'
 
@@ -36,7 +49,9 @@
             //      format will be similar to the above IF statement.
 
 
-        };
+        }
+
+
 
         var pass = 'the RegEx .test statement is needed here';
         var errorMsg = inputName.nextSibling.nextSibling.nextSibling.nextSibling;
