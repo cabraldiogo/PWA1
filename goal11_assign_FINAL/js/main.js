a047dd2de45d4bf4786655358bdf38f438634ad8
cabraldiogo <cabraldiogo@me.com>
2013-10-24 13:47:10 -0400

    Comments on the code

--- goal11_assign_FINAL/js/main.js
+++ goal11_assign_FINAL/js/main.js
@@ -14,24 +14,26 @@
     var students = [];
 
     var i = 0;
-    var d = new Date();
-    var dd = d.getDate();
-    var mm = d.getMonth();
-    var yyyy = d.getFullYear().toString().substr(2,2);
+    var d = new Date();  // returns the date showing year, month, day, hours, minutes, seconds, milliseconds
+    var dd = d.getDate(); // returns the day of the month
+    var mm = d.getMonth(); // returns the month and is zero index
+    var yyyy = d.getFullYear().toString().substr(2,2);// returns the year with 2 digits
 
 
-    var btn = document.querySelector("#info_btn");
+    var btn = document.querySelector("#info_btn"); // Created btn variable and assigned it the the button ID
 
 
-    btn.addEventListener("click",displayData);
+    btn.addEventListener("click",displayData); // Assigns a function to the button
 
-
+    // creates a student object using the constructor   and insert it into the student array
     var diogo = new Student("Diogo Cabral", {street:"University Blvd", city:"Winter Park", state:"FL"},[3.0,3.0,3.0],new Date());
     students.push(diogo);
 
+    // creates a student object using the constructor  and insert it into the student array
     var gabriel = new Student("Gabriel Ferraz", {street:"Semoran Blvd", city:"Orlando", state:"FL"},[3.5,2.0,3.0],new Date());
     students.push(gabriel);
 
+    //Prototype to get the GPA average
     Student.prototype.getAverage = function(g){
         var total=0;
         g.forEach(function(e){
@@ -43,6 +45,7 @@
         return gpaAvg.toFixed(2);
     };
 
+    //Function to console.log the students info getting the information from the student array
     function studentsInfo(){
         for(var key in students){
 
@@ -80,19 +83,19 @@
 
 
 
-//Console.log the first two students info
+//Console.log the first two students info calling the studentsInfo function
     console.log("*** Below is the original 2 students ***" + "\n");
     studentsInfo();
 
-
+    // creates a student object using the constructor  and insert it into the student array
     var kareem = new Student("Kareem Beazer",{street:"University Blvd", city:"Winter Park", state:"FL"},[2.0,3.5,4.5],new Date());
     students.push(kareem);
 
-//Console.log the new student Array that contains the new student
+//Console.log the new students array info calling the studentsInfo function.
     console.log("*** Below is the new object with a new student on the object ***" + "\n");
     studentsInfo();
 
-
+    //Call the function that display the student info in the HTML
     displayData();
 
 
